6d6f6984a0bb2a7c395928efc13f1805cb8196e7
Reid Booth <Reid.Booth@nv5.com>
2026-03-09 16:57:40 -0700

    More refinement and tweaks to the algorithm for clarity of code and function

--- DuneImperiumEquationBalancer/Program.cs
+++ DuneImperiumEquationBalancer/Program.cs
@@ -1,110 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using DuneImperiumEquationBalancer;
-using System.Collections.Immutable;
-using System.Security.Cryptography.X509Certificates;
-
-//Console.WriteLine("Hello, World!");
 
 List<DuneImperiumEquationBalancer.Space> spaces = new List<DuneImperiumEquationBalancer.Space>();
 
-/*
-Space 'costs'
-
-Worker Spent
-Solari
-Spice
-Water
-Faction 2-req
-Faction Space
-Once/game
-Green Space
-Blue Space
-Yellow Space
-
-Space 'Benefits'
-
-
-1  Solari
-2  Spice
-3  Water
-4  Faction bump
-5  Draw Intrigue
-6  Gain Troops
-7  Combat Allowed
-8  Draw Card
-9  Draw Contract
-10 Gain Spy
-11 Steal intrigues
-12 Trash
-13 Permanent 2 influence
-14 Permanent Worker
-15 Spice Accumulation
-16 Shipping
-17 foldspace
-18 mentat
-19 temporary influence
-20 discount/negotiator
-21 buy tech
-22 get dreadnought
-23 solari flag
-24 spice flag
-25 microscope*/
-/*26 double DiscardDrawIntrigue = 1.0;
-  27  double Contract = 2.0;
-  28  double PickUpWorker = 1.0;
-  29  double Spy = 1.5;
-  30  double TwoSpiceOrDeploy = 1.0;
-  31  double FourSpiceOrDeployTwo = 1.0;
-  32  double GetMakerHook = 1.0;
-  33  double DestroyShieldWall = 1.0;
-  34  double WildBump = 1.0;*/
-/*double Goingtospace = 2.6;
-    double SolariValue = 1;
-    double SpiceValue = 1.7;
-    double WaterValue = 1.2;
-    double DrawCard = 1.8;
-    double DrawIntrigue = 1.6;
-    double Shipping = 1.8;
-    double StealIntrigue = 0.6;
-    double FactionBump = 2;
-    double GainTroops = 1.8;
-    double Combat = 1;
-    double Trash = 2;
-    double HighCouncil = 8;
-    double Swordmaster = 11;
-    double SpiceAccumulation = 1.1;
-    double Mentat = 3.3;
-    double Faction2req = 0.6;
-    
-    double Oncegame = 0.1;
-    double Foldspace = 2.2;
-    double tempinfluence = 1.1;
-    double solariflag = -1;
-    double spiceflag = 0.2;
-    double microscope = 1.6;
-    double FactionSpace = 1.6;
-    double GreenSpace = 0.3;
-    double BlueSpace = 0.8;
-    double Yellowspace = 0.4;
-
-    //Uprising-specific benefits
-    double DiscardDrawIntrigue = 1.9;
-    double Contract = 2.0;
-    double PickUpWorker = 3.0;
-    double Spy = 2.3;
-    double TwoSpiceOrDeploy = 3.1;
-    double FourSpiceOrDeployTwo = 5.5;
-    double GetMakerHook = 0.0;
-    double DestroyShieldWall = 1.8;
-    double WildBump = 3.7;*/
-//string[] CostNames = new string[] { "Goingtospace", "SolariValue", "SpiceValue", "WaterValue", "Faction2req", "FactionSpace", "Oncegame", "GreenSpace", "BlueSpace", "YellowSpace", "DrawIntrigue" };
-//string[] GainNames = new string[] { "SolariValue", "SpiceValue", "WaterValue", "FactionBump", "DrawIntrigue", "GainTroops", "Combat", "DrawCard", "null", "null", "StealIntrigue", "Trash", "HighCouncil", "Swordmaster", "SpiceAccumulation", "Shipping", "Foldspace", "Mentat", "tempinfluence", "TechDiscountNegotiator", "BuyTech", "GetDreadnought", "solariflag", "spiceflag", "microscope", "DiscardDrawIntrigue", "Contract", "PickUpWorker", "Spy", "TwoSpiceOrDeploy", "FourSpiceOrDeployTwo", "GetMakerHook", "DestroyShieldWall", "WildBump" };
-//string[] DescriptiveCostNames = new string[] { "Sending worker to a space", "Solari", "Spice", "Water", "Has Faction 2 influence requirement", "Is a faction space", "Can only go once/game", "Is a green space", "Is a blue space", "Is a yellow space", "Draw an Intrigue" };
-//[] DescriptiveGainNames = new string[] { "Solari", "Spice", "Water", "Faction-specific Bump", "Draw an Intrigue", "Gain Troop", "Space allows combat", "Draw a Card", "null", "null", "Steal intrigues above 4", "Trash a card", "High Council", "Swordmaster", "Spice Accumulation", "Shipping", "Foldspace", "Mentat", "Temporary influence", "Buy Tech w/ Discount or Negotiator", "Buy Tech", "Get Dreadnought", "Has solari flag", "Has spice flag", "Microscope", "Discard and Draw an Intrigue", "Contract", "Pick Up Worker", "Spy", "Two Spice Or Deploy Worm", "Four Spice Or Deploy Two Worm", "Get Maker Hook", "Destroy Shield Wall", "Wild Faction Bump" };
-
-//base game board spaces
-//                                                                                                                                              X              X              X              X              X              X              X
-//spaces.Add(new DuneImperiumEquationBalancer.Space() { Costs = new double[] { 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0 }, Gains = new double[] { 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, Name = "Arrakeen" });
 spaces.Add(new Space("Arrakeen")
     .SetIsInAll()
     .Default(ID.BlueSpace)
@@ -320,6 +218,8 @@ spaces.Add(new Space("Dreadnought")
     .Gain(ID.GetDreadnought, 1)
     .Gain(ID.BuyTech, 1)
      );
+
+//Uprising spaces
 spaces.Add(new Space("Spice Refinery 1")
     .SetIsUprising()
     .Default(ID.BlueSpace)
@@ -492,6 +392,8 @@ spaces.Add(new Space("Gather Support 2")
     .Gain(ID.WaterValue, 1)
     .Gain(ID.GainTroops, 2)
      );
+
+//Intrigues
 spaces.Add(new Space("Windfall")
     .SetIsInAllBaseNotUprising()
     .IntrigueDefault()
@@ -592,8 +494,14 @@ spaces.Add(new Space("Bribery")
     .Cost(ID.SolariValue, 2)
     .Gain(ID.WildBump, 1)
      );
+spaces.Add(new Space("Advanced Weaponry")
+    .SetIsInAllBaseNotUprising()
+    .IntrigueDefault()
+    .Cost(ID.SolariValue, 3)
+    .Gain(ID.GetDreadnought, 1)
+     );
 
-
+//Uprising intrigues
 spaces.Add(new Space("Detonation 1")
     .SetIsUprising()
     .IntrigueDefault()
@@ -702,12 +610,26 @@ RunBalancer();
 void RunBalancer()
 {
     double[] weights = { 2.6, 1, 1.56, 1.46, 0.6, 1.6, 0.1, 0.3, 0.58, 0.7, 1.6, 2, 1.58, 0.74, 1.59, 0.6, 2.14, 8, 11, 0.9, 2, 2.2, 3.3, 1.29, 1.61, 1.6, 4.3, -0.73, 0.1, 1.6, 2.91, 2.1, 2, 2.17, 3.12, 6.04, 0, 1.6, 4.6 };
+    double[] commonality_weights = { 0, 0.3977, 0.2727, 0.1591, 0, 0, 0, 0, 0, 0, 0, 0, 0.6023, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }; // currently unused, figure out scarcity
+
+    /*Console.Write("{");
+    for (int i = 0; i < commonality_weights.Length; i++)
+    {
+        commonality_weights[i] = CalculateAverageOfSpacesThatProvide((ID)i);
+        Console.Write($"{Math.Round(CalculateAverageOfSpacesThatProvide((ID)i),3)}");
+        if ((i + 1 < commonality_weights.Length))
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.Write("};\n");*/
+
 
     double balance_factor = 0.01;
 
     for(int j = 0; j < 200; j++)
     {
-        double base_balance = TotalBalance(weights);
+        double base_balance = TotalBalance(weights, commonality_weights);
         Console.WriteLine($"Base balance: {base_balance}");
         double biggest_difference = 0;
         bool gain = false;
@@ -718,15 +640,15 @@ void RunBalancer()
             double[] _weights = new double[weights.Length];
             Array.Copy(weights, _weights, weights.Length);
             _weights[i] += balance_factor;
-            if (TotalBalance(_weights) < base_balance)
+            if (TotalBalance(_weights, commonality_weights) < base_balance)
             {
-                if(base_balance - TotalBalance(_weights) > biggest_difference)
+                if(base_balance - TotalBalance(_weights, commonality_weights) > biggest_difference && i!=1)
                 {
-                    biggest_difference = base_balance - TotalBalance(_weights);
+                    biggest_difference = base_balance - TotalBalance(_weights, commonality_weights);
                     gain = true;
                     index_to_change = i;
                 }
-                Console.WriteLine($"Increasing weight {(ID)i} improved balance by {Math.Round(base_balance - TotalBalance(_weights), 2)}");
+                Console.WriteLine($"Increasing weight {(ID)i} improved balance by {Math.Round(base_balance - TotalBalance(_weights, commonality_weights), 2)}");
             }
         }
         for (int i = 0; i < weights.Length; i++)
@@ -734,15 +656,15 @@ void RunBalancer()
             double[] _weights = new double[weights.Length];
             Array.Copy(weights, _weights, weights.Length);
             _weights[i] -= balance_factor;
-            if (TotalBalance(_weights) < base_balance)
+            if (TotalBalance(_weights, commonality_weights) < base_balance)
             {
-                if (base_balance - TotalBalance(_weights) > biggest_difference)
+                if (base_balance - TotalBalance(_weights, commonality_weights) > biggest_difference && i != 1)
                 {
-                    biggest_difference = base_balance - TotalBalance(_weights);
+                    biggest_difference = base_balance - TotalBalance(_weights, commonality_weights);
                     gain = false;
                     index_to_change = i;
                 }
-                Console.WriteLine($"Decreasing weight {(ID)i} improved balance by {Math.Round(base_balance - TotalBalance(_weights), 2)}");
+                Console.WriteLine($"Decreasing weight {(ID)i} improved balance by {Math.Round(base_balance - TotalBalance(_weights, commonality_weights), 2)}");
             }
         }
 
@@ -759,21 +681,32 @@ void RunBalancer()
                 weights[index_to_change] -= balance_factor;
             }
         }
+        else
+        {
+            Console.WriteLine("No improvement found, exiting early");
+            break;
+            
+        }
     }
     
     SortedDictionary<string, double> space_balances = new SortedDictionary<string, double>();
     double[] balances = new double[spaces.Count];
     for (int i = 0; i < spaces.Count; i++)
     {
-        Console.WriteLine($"Calculating balance for space {spaces[i].Name()}");
-        balances[i] = SpaceBalance(spaces[i], weights);
-        Console.WriteLine(Math.Round(balances[i],2));
+        //Console.WriteLine($"Calculating balance for space {spaces[i].Name()}");
+        balances[i] = SpaceBalance(spaces[i], weights, commonality_weights);
+        //Console.WriteLine(Math.Round(balances[i],2));
         space_balances.Add(spaces[i].Name(), balances[i]);
     }
     Dictionary<string, double> space_balances_sorted = space_balances.OrderBy(x => x.Value).ToDictionary();
+
+    Console.WriteLine("\nSpaces/intrigues ranked for balance:");
     foreach (var space in space_balances_sorted) {
         Console.WriteLine($"{space.Key}: {Math.Round(space.Value,2)}");
     }
+
+    //outputs weights in a format that can be copy-pasted into the code
+    Console.WriteLine("\nFinal weights in array format:");
     Console.Write("{");
     for (int i = 0; i < weights.Length; i++)
     {
@@ -784,6 +717,7 @@ void RunBalancer()
         }
     }
     Console.Write("};\n");
+    Console.WriteLine("\nFinal weights:");
     for (int i = 0; i < weights.Length; i++)
     {
         Console.WriteLine($"{Math.Round(weights[i],3)}\t - {(ID)i}");
@@ -801,25 +735,39 @@ void RunBalancer()
 
 }
 
-double TotalBalance(double[] weights)
+double TotalBalance(double[] weights, double[] commonality_weights)
 {
     double balance = 0;
     foreach (var space in spaces)
     {
-        balance += Math.Abs(SpaceBalance(space, weights));
+        balance += Math.Abs(SpaceBalance(space, weights, commonality_weights));
     }
     return balance;
 }
 
-double SpaceBalance(Space space, double[] weights)
+double SpaceBalance(Space space, double[] weights, double[] commonality_weights)
 {
     double balance = 0;
     for (int i = 0; i < weights.Length; i++)
     {
-        balance -= space.GetGains()[i] * weights[i];
-        balance += space.GetCosts()[i] * weights[i];
+        balance -= (space.GetGains()[i] * (weights[i]));
+        balance += (space.GetCosts()[i] * (weights[i]));
     }
 
     return balance;
+}
+
+double CalculateAverageOfSpacesThatProvide(ID i)
+{
+    double total = 0;
+    int spaces_checked = 0;
+    foreach (var space in spaces) { 
+        if(space.GetCost(ID.DrawIntrigue) == 0) // not an intrigue
+        {
+            spaces_checked++;
+            total += space.GetGains()[(int)i];
+        }
+    }
+    return total/spaces_checked;
 }
 
---
+++ DuneImperiumSpacesGenerator/Board.cs
@@ -0,0 +1,35 @@
+﻿namespace DuneImperiumSpacesGenerator
+{
+    public interface IBoard
+    {
+        List<Space> Spaces { get; }
+        //allows the return of the generated spaces
+        void SetSpaceRandomizationConstraints(SpaceRandomizationConstraints constraints);
+        //allows the user to pass a set of constraints on generation
+        bool TryRandomizeSpaces();
+        //try to actually generate spaces, returning false if the constraints are impossible to satisfy
+
+    }
+
+    public class DuneImperiumBoard : IBoard
+    {
+        public List<Space> Spaces { get; set; }
+        private SpaceRandomizationConstraints Constraints { get; set; }
+
+        public DuneImperiumBoard()
+        {
+            Spaces = new List<Space>();
+            Constraints = new SpaceRandomizationConstraints();
+        }
+
+        public void SetSpaceRandomizationConstraints(SpaceRandomizationConstraints _constraints)
+        {
+            Constraints = _constraints;
+        }
+
+        public bool TryRandomizeSpaces()
+        {
+            return true;
+        }
+    }
+}
